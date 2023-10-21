f0ad814ef2709eea599c9c9a7881db845c16f884
leminence <lema.repostory@gmail.com>
2023-10-21 18:45:10 -0300

    ecocosta v1

--- script.js
+++ script.js
@@ -1,7 +1,28 @@
-menubotao = document.getElementById('menubutton')
-menu = document.getElementById('menu')
-menubotao.addEventListener('click', abrirmenu)
-
-function abrirmenu() {
-    menu.style.width = "50vh"
+class Post {
+    constructor() {
+        this.imagem = imagem;
+        this.idpost = idpost;
+        this.autor = autor;
+        this.nome = nome;
+        this.especie = especie;
+    }
 }
+let postexpandido = document.getElementById('visualizarpost') 
+document.getElementById('fechar').addEventListener('click', function () {
+    postexpandido.style.display = "none"
+})
+let gridposts = document.getElementById("gridposts")
+function criarposts () {
+    for (let i = 0; i < 30; i++) {
+        let miniatura = document.createElement('div');
+        miniatura.innerHTML = i
+        miniatura.setAttribute('id', 'post' + i );
+        miniatura.addEventListener("click", function () {
+            document.getElementById('postminiatura').innerHTML = "<p>"+this.innerHTML+"<p>"
+            postexpandido.style.display = "flex"
+        })
+        document.getElementById('gridpost').appendChild(miniatura);
+    }
+}
+
+criarposts()
